9d2c63d2092989d00354a02a3065aed7ca20ddd9
guilherme <guiduffes@gmail.com>
2023-08-15 09:26:52 -0300

    third

--- src/components/section.js
+++ src/components/section.js
@@ -3,9 +3,33 @@ import '../css-c/section.css';
 
 export default function Main() {
   return (
-    <div className='main'>
+    <div className='mainSection'>
         <div className="skills">
             <div className='firstSkill' >
+                <div className='infos' >
+                
+                </div>
+            </div>
+
+            <div className='firstSkill second' >
+                <div className='infos' >
+                
+                </div>
+            </div>
+
+            <div className='firstSkill third' >
+                <div className='infos' >
+                
+                </div>
+            </div>
+
+            <div className='firstSkill fourth' >
+                <div className='infos' >
+                
+                </div>
+            </div>
+
+            <div className='firstSkill fifith ' >
                 <div className='infos' >
                 
                 </div>
