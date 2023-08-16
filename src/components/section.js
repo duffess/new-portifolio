de7d64cba919d7c1f035b25251485eb6fbba89c1
guilherme <guiduffes@gmail.com>
2023-08-15 21:58:53 -0300

    test

--- src/components/section.js
+++ src/components/section.js
@@ -1,38 +1,49 @@
 import React from 'react';
 import '../css-c/section.css';
+import ImgReact from "../assets/imgs/react.png"
+import JsImg from "../assets/imgs/js.png"
+import NativeImg from "../assets/imgs/native.png"
+import NodeImg from "../assets/imgs/node.png"
+import ExpImg from "../assets/imgs/review.png"
+
 
 export default function Main() {
   return (
     <div className='mainSection'>
         <div className="skills">
             <div className='firstSkill' >
-                <div className='infos' >
-                
-                </div>
+                <img src={ImgReact}/>
+                 <div className='infos' >
+                    <p>React.Js</p>
+                    </div>
             </div>
 
             <div className='firstSkill second' >
-                <div className='infos' >
-                
-                </div>
+                <img src={JsImg}/>
+                    <div className='infos' >
+                        <p>JavaScript</p>
+                    </div>
             </div>
 
             <div className='firstSkill third' >
-                <div className='infos' >
-                
-                </div>
+                <img src={NativeImg}/>
+                    <div className='infos' >
+                        <p>React Native</p>
+                    </div>
             </div>
 
             <div className='firstSkill fourth' >
-                <div className='infos' >
-                
-                </div>
+                <img src={NodeImg}/>
+                    <div className='infos' >
+                        <p>Node.Js</p>
+                    </div>
             </div>
 
             <div className='firstSkill fifith ' >
-                <div className='infos' >
-                
-                </div>
+                <img src={ExpImg}/>
+                    <div className='infos' >
+                        <p>Skill Extra</p>
+                    </div>
             </div>
         </div>
     </div>
