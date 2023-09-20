e0079af402860bbd1e8d6b82fe0d9145ac9b79cf
guilherme <guiduffes@gmail.com>
2023-09-20 13:24:10 -0300

    about me

--- src/components/section2.js
+++ src/components/section2.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../css-c/section2.css';
-import ImgEu from "../assets/imgs/Vector_1 (1).webp"
+import ImgEu from "../assets/imgs/Novo Projeto.webp"
 
 export default function Main() {
 
@@ -8,14 +8,25 @@ export default function Main() {
 
     return (
         <div className='mainSection2'>
+
+            <div className='PaiSobreMim' >
+                <h1>Sobre Mim</h1>
+                <div></div>
+            </div>
+
+
+            <div className='PaiTextosImg'>
+
+                
             <img src={ImgEu} alt="Imagem de Guilherme" />
+
 
             <div className='textos'>
 
                 <h2>Olá! Eu sou Guilherme Duffes</h2>
                 <h3>Desenvolvedor Front-End</h3>
                 <div className='DivDescricao'>
-                    <span>Sou um desenvolvedor Front-End altamente qualificado, especializado na biblioteca React. Com ampla experiência em desenvolvimento web, destaco-me no uso das melhores práticas do React e mantenho-me atualizado com as últimas tendências da indústria. Proficiente em HTML, CSS, JavaScript e ferramentas de compilação, estou dedicado a entregar projetos de alta qualidade e de ponta. Meu compromisso com a qualidade e inovação me diferencia e me torna um ativo valioso para qualquer equipe.</span>
+                    <span>Sou um desenvolvedor Front-End especializado em React, com experiência em desenvolvimento web e práticas modernas. Sou proficiente em HTML, CSS, JavaScript e ferramentas de compilação. Comprometido em entregar projetos de alta qualidade e inovadores.</span>
                 </div>
                 <div className='texto1'>
 
@@ -28,6 +39,8 @@ export default function Main() {
                 </div>
 
             </div>
+            </div>
+
         </div>
     );
 }
