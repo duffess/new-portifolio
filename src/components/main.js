399a9abfabdad71b5d18bdf39501d9a9ae4b4812
guilherme <guiduffes@gmail.com>
2023-10-09 01:06:32 -0300

    main done

--- src/components/main.js
+++ src/components/main.js
@@ -7,15 +7,12 @@ export default function Main() {
     <div className="main">
         <div className='subMain'>
             <div className='presentMyself' >
-                <p className='Apre' > OLÁ, </p>
-                <p className='Apre' > EU SOU <br></br> GUILHERME DUFFES</p>
-                <span> Atualmente progredindo nos estudosna área de desenvolvimento no instituto Infnet, focando principalmente no desenvolvimento front-end com react e ferramentas. Além de estar muito determinado para meu crescimento pessoal de conhecimento na área da tecnologia, criando projetos sempre inovadores para mim com muito amor e dedicação. </span>
-                <button>Ola</button>
+                <p className='Apre' >DUFFES</p>
+                <span> Estou estudando desenvolvimento no Instituto Infnet, com foco em front-end usando React e ferramentas. Tenho determinação em crescer na área de tecnologia e criar projetos inovadores. </span>
+                <button className='button' > Entre em contato </button>
             </div>
 
-            <div className='imgLamp'>
-                <img src={lamp} />
-            </div>
+            
         </div>
     </div>
   );
